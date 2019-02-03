97e2d907f6c236969b13e7a700cec3c97cfedb0a
leoiphonedev <aggarwalaman1985@gmail.com>
2019-02-03 16:36:25 +0530

    Pushing code

--- ImageDownLoadExample/ViewController.swift
+++ ImageDownLoadExample/ViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import Kingfisher
 
 class ViewController: UIViewController {
+    @IBOutlet weak var imgvHeader: UIImageView!
+    @IBOutlet weak var imgvHeader1: UIImageView!
+    @IBOutlet weak var imgvHeader2: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        imgvHeader.kf.indicatorType = .activity
+        imgvHeader.kf.setImage(with: URL(string: "https://cdn.pixabay.com/photo/2016/02/05/04/49/australia-1180394_1280.jpg"), placeholder: nil, options: [.transition(.fade(0.7))], progressBlock: nil)
     }
 
 
